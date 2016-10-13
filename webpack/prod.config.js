7ef4d104f0e5442ae97cf14ad2d71a23903231a8
Jameson Knoll <j.knoll621@gmail.com>
2016-10-13 15:16:21 +0800

    rebuild

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,7 +1,19 @@
 /* eslint import/no-extraneous-dependencies: 0 */
+
 const path = require('path');
 const webpack = require('webpack');
+
 const assetsPath = path.resolve(__dirname, '../public/assets');
+
+const webpackIsomorphicToolsConfig = require('./webpack-isomorphic-tools');
+const WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+
+const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(webpackIsomorphicToolsConfig);
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   devtool: 'source-map',
   context: path.resolve(__dirname, '..'),
@@ -12,7 +24,7 @@ module.exports = {
   },
   output: {
     path: assetsPath,
-    filename: '[name].js',
+    filename: '[name]-[hash].js',
     chunkFilename: '[name]-[chunkhash].js',
     publicPath: '/assets/',
   },
@@ -23,7 +35,41 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
       },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+                  'style',
+                  'css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss'
+                ),
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml',
+      },
+      {
+        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+        loader: 'url-loader?limit=10240',
+      },
     ],
+  },
+  postcss() {
+    return [autoprefixer];
   },
   progress: true,
   resolve: {
@@ -34,15 +80,20 @@ module.exports = {
     extensions: ['', '.json', '.js', '.jsx'],
   },
   plugins: [
+    new ExtractTextPlugin('[name]-[chunkhash].css', { allChunks: true }),
+
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"',
       },
+
       __CLIENT__: true,
       __DEVTOOLS__: false,
     }),
+
     // ignore dev config
     new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
+
     // optimizations
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
@@ -51,5 +102,6 @@ module.exports = {
         warnings: false,
       },
     }),
+    webpackIsomorphicToolsPlugin,
   ],
 };
